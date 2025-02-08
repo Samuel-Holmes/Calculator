9e602121e9b6fdf0a82f9841f2587b878fb655f3
Samuel-Holmes <samuel_n_holmes@outlook.com>
2025-02-08 22:43:17 +0000

    Create basic calculator structure and logic

---
+++ source.py
@@ -0,0 +1,11 @@
+# Simple Calculator Programme 
+
+running = True
+
+while running:
+    print("Hello welcome to the calculator if you would like to exit at any time then please enter 'n'")
+
+    Calculation = input("Input the calculation you would like to make here please: ")
+
+    if Calculation == "n":
+        break

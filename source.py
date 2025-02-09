c30dc9be449ab3c01e2745a0d4add0497f8e5cf8
Samuel-Holmes <samuel_n_holmes@outlook.com>
2025-02-09 00:43:58 +0000

    Establish conditional logic for user input

--- source.py
+++ source.py
@@ -1,18 +1,46 @@
 # Simple Calculator Programme 
 
+
+# Setting the boolean for the while loop of the programme 
+
 running = True
 
-while running:
-    print("Hello welcome to the calculator if you would like to exit at any time then please enter 'n'")
+print("Hello welcome to the calculator if you would like to exit at any time then please enter 'n'")
 
+# opening the loop 
+
+while running:
+    
+
+    
     # programme needs to take user input and an operand 
 
-    num1, num2 = input("Please enter the first number followed by a space then the second number: ").split()
+    num1 = input("Please enter the first number: ")
+    
+    if num1 == 'n':
+        break
+    
+    else:
+        num2 = input("Please enter the second number: ")
 
+
+    num1 = int(num1)
+    num2 = int(num2)
 
     operand = input("Please enter the operand you require (*, /, +, -): ")
 
     # basic input taken now needs the conditional logic implemented
-   
-    if Calculation == "n":
-        break
+
+    if operand =="*":
+        print(f"The answer is: {num1 * num2}")
+    
+    if operand == '/':
+        print(f"The answer is: {num1 / num2}")
+
+    if operand == '+':
+        print(f"The answer is: {num1 + num2}")
+
+    if operand == '-':
+        print(f"The answer is: {num1 - num2}")
+
+    

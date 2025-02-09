b0864b35c76b910ac3cc31a2c2cc2ae2e4f329e3
Samuel-Holmes <samuel_n_holmes@outlook.com>
2025-02-09 00:03:24 +0000

    Take user input for operand and two numbers

--- source.py
+++ source.py
@@ -5,7 +5,14 @@ running = True
 while running:
     print("Hello welcome to the calculator if you would like to exit at any time then please enter 'n'")
 
-    Calculation = input("Input the calculation you would like to make here please: ")
+    # programme needs to take user input and an operand 
 
+    num1, num2 = input("Please enter the first number followed by a space then the second number: ").split()
+
+
+    operand = input("Please enter the operand you require (*, /, +, -): ")
+
+    # basic input taken now needs the conditional logic implemented
+   
     if Calculation == "n":
         break

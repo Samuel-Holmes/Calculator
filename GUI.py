d240aca5728e43418d3cbcc6ac78820856a05f36
Samuel-Holmes <samuel_n_holmes@outlook.com>
2025-02-11 12:17:15 +0000

    Plan GUI and GUI integration with main programme

--- GUI.py
+++ GUI.py
@@ -17,3 +17,19 @@ ttk.Button(frm, text ="1", value = '1').grid(column =0, row = 0)
 # loop to put everything on display until programme termination 
 
 root.mainloop()
+
+
+
+''' Proposed planning 
+
+In my mind to allow more complex calculations I need to assign values to the tkinter buttons and then append them to a string using a callback function
+
+This can then be typecast to an integer and used for calculations 
+
+if I want to extend the complexity further to more than two numbers, more than one operand, there will need to be logic for initialising a new variable each time an operand is clicked 
+
+I should add the explicit click of an equals button as an endpoint this will make programming the calculation point easier 
+
+'''
+
+ 
